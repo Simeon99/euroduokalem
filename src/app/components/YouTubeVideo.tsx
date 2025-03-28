28c4eb50eae4c67b95423141289374bab922a090
Simeon99 <simeonilic@yahoo.com>
2025-03-28 18:18:31 +0100

    Fix YouTube player

--- src/app/components/YouTubeVideo.tsx
+++ src/app/components/YouTubeVideo.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const YouTubeVideo = () => {
     return (
-        <div className="w-full max-w-3xl mx-auto aspect-video rounded-xl overflow-hidden shadow-lg">
+        <div className='flex flex-col px-4 '>
+        <div className="max-w-screen-sw  w-full  mx-auto aspect-video rounded-xl overflow-hidden shadow-lg">
             <iframe
                 className="w-full h-full"
-                src="https://www.youtube.com/watch?v=vHbs0XlD4A4&t=43s"
+                src="https://www.youtube.com/embed/vHbs0XlD4A4"
                 title="YouTube video"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
             ></iframe>
+        </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HomeMessage from "./components/HomeMessage";
 import HomeProducts from "./components/HomeProducts";
 import HomeSeedlings from "./components/HomeSeedlings";
 import Landing from "./components/Landing";
+import YouTubeVideo from "./components/YouTubeVideo";
 
 export default function Home() {
   return (
@@ -24,16 +25,8 @@ export default function Home() {
       <div className="pt-[140px] pb-[140px]">
         <HomeMessage />
       </div>
-      <div>
-        <div className="w-full max-w-3xl mx-auto aspect-video rounded-xl overflow-hidden shadow-lg">
-          <iframe
-            className="w-full h-full"
-            src="https://www.youtube.com/watch?v=vHbs0XlD4A4&t=43s"
-            title="YouTube video"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
+      <div className="pb-[140px]">
+        <YouTubeVideo />
       </div>
 
     </div>
