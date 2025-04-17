9bfecca5cf19669f34e96e9b4b3ab39399ff067c
Simeon99 <simeonilic@yahoo.com>
2025-04-17 17:04:09 +0200

    Fix Seedling page error

--- src/app/[lang]/seedlings/[seedling]/page.tsx
+++ src/app/[lang]/seedlings/[seedling]/page.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Seedling from '@/app/components/Seedling';
+import { Locale } from '../../dictionaries';
 
-export default async function FruitPage({
-  params,
-}: {
-  params: { seedling: string; lang: string };
-}) {
-  const { seedling, lang } = params;
+interface PageProps {
+  params: Promise<{
+    lang: Locale;
+    seedling: string;
+  }>;
+}
+
+
+
+export default async function FruitPage ({ params }: PageProps) {
+  const { lang } = await params;
+  const { seedling } = await params;
+  
+  // const t = await getDictionary(lang);
 
   return (
-    <div className='flex justify-center px-4'>
-      <div className='max-w-screen-sw  w-full '>
+    <div className="flex justify-center px-4">
+      <div className="max-w-screen-sw w-full">
         <Seedling fruit={seedling} lang={lang} />
       </div>
     </div>
