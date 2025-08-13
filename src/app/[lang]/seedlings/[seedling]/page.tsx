3dd8bde64a68d8e33d9fbe4960e2b8081a46aab0
Simeon99 <simeonilic@yahoo.com>
2025-08-13 22:43:41 +0200

    Thumbnail seedling.

--- src/app/[lang]/seedlings/[seedling]/page.tsx
+++ src/app/[lang]/seedlings/[seedling]/page.tsx
@@ -9,6 +9,27 @@ interface PageProps {
   }>;
 }
 
+const fruitImages: Record<SeedlingKey, string> = {
+    apple: 'https://euroduokalem.com/images/seedlings/apple.jpg',
+    pear: 'https://euroduokalem.com/images/seedlings/pear.jpg',
+    plum: 'https://euroduokalem.com/images/seedlings/plum.jpg',
+    'sweet-cherry': 'https://euroduokalem.com/images/seedlings/sweet-cherry.jpg',
+    'sour-cherry': 'https://euroduokalem.com/images/seedlings/sour-cherry.jpg',
+    apricot: 'https://euroduokalem.com/images/seedlings/apricot.jpg',
+    peach: 'https://euroduokalem.com/images/seedlings/peach.webp',
+    'columnar-apple': 'https://euroduokalem.com/images/seedlings/columnar-apple.jpg',
+    quince: 'https://euroduokalem.com/images/seedlings/quince.jpg',
+    medlar: 'https://euroduokalem.com/images/seedlings/medlar.jpg',
+    nectarine: 'https://euroduokalem.com/images/seedlings/nectarine.jpg',
+    hazelnut: 'https://euroduokalem.com/images/seedlings/hazelnut.jpg',
+    walnut: 'https://euroduokalem.com/images/seedlings/walnut.jpg',
+    grapevine: 'https://euroduokalem.com/images/seedlings/purple.jpg',
+    gooseberry: 'https://euroduokalem.com/images/seedlings/gooseberry.jpg',
+    currant: 'https://euroduokalem.com/images/seedlings/currant.jpg',
+    blackberry: 'https://euroduokalem.com/images/seedlings/berry.jpg',
+    raspberry: 'https://euroduokalem.com/images/seedlings/raspberry.jpg',
+    roses: 'https://euroduokalem.com/images/seedlings/roses.jpg',
+  };
 
 type SeedlingKey =
   | 'apple'
@@ -16,6 +37,7 @@ type SeedlingKey =
   | 'plum'
   | 'sweet-cherry'
   | 'sour-cherry'
+  | 'apricot'
   | 'peach'
   | 'columnar-apple'
   | 'quince'
@@ -34,6 +56,7 @@ export async function generateMetadata({ params }: { params: Promise<{ lang: Loc
    const { lang, seedling } = await params;
   const t = await getDictionary(lang);
   const fruitName = seedling as SeedlingKey;
+  const selectedImage = fruitImages[fruitName];
 
   return {
     title: t.seo.seedling[fruitName].title,
@@ -41,7 +64,15 @@ export async function generateMetadata({ params }: { params: Promise<{ lang: Loc
     keywords: t.seo.seedling[fruitName].keywords,
     openGraph: {
       title: t.seo.seedling[fruitName].openGraph.title,
-      description: t.seo.seedling[fruitName].openGraph.description
+      description: t.seo.seedling[fruitName].openGraph.description,
+      images: [
+        {
+          url: selectedImage,
+          width: 1200,
+          height: 630,
+          alt: t.seo.seedling[fruitName].title,
+        },
+      ],
     }
   };
 }
