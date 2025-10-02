d7429f250dc43fa3797500411fc9ca1867994dde
Simeon99 <simeonilic@yahoo.com>
2025-10-02 20:13:49 +0200

    Add CLARITY tracker to website

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -74,6 +74,15 @@ export default async function RootLayout({ children, params }: PageProps) {
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=G-G2Y2T899PT`} // 👈 zameni svojim Measurement ID-jem
         />
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+          `}
+        </Script>
         <Script
           id="gtag-init"
           strategy="afterInteractive"
